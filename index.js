795eb9ea7587f3b845779c27e9e2bc08979bd721
Xavier <xavier.garces@devex.com>
2019-12-17 17:14:45 +0100

    3.16: Phonebook database, step3

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const app = express();
+
 const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path);
+  console.log('Body:  ', request.body);
+  console.log('---');
+  next();
+}
+
+app.use(requestLogger);
+
 const morgan = require('morgan');
 const cors = require('cors');
 const Person = require('./models/person');
 
-app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static('build'))
+app.use(express.static('build'));
 
 morgan.token('customToken', function (tokens, req, res) {
   return [
@@ -31,12 +44,16 @@ app.get('/info', (req, res) => {
   );
 });
 
-app.get('/api/persons', (request, response) => {
+// Get Persons
+app.get('/api/persons', (request, response, next) => {
   Person.find({}).then(persons => {
     response.json(persons.map(person => person.toJSON()))
-  });
+  })
+  .catch(error => next(error));
 });
+// Get Persons
 
+// Get Person Method
 app.get('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id);
   const person = persons.find(person => person.id === id);
@@ -47,8 +64,10 @@ app.get('/api/persons/:id', (req, res) => {
     res.status(404).end();
   }
 });
+// Get Person Method
 
-app.post('/api/persons', (request, response) => {
+// Post method
+app.post('/api/persons', (request, response, next) => {
   const body = request.body;
 
   if(!body.name || !body.number) {
@@ -64,16 +83,34 @@ app.post('/api/persons', (request, response) => {
 
   person.save().then(savedPerson => {
     response.json(savedPerson.toJSON());
-  });
+  })
+  .catch(error => next(error))
 });
+// Post method
 
-
+// Delete Method
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
     .then(result => {
       response.status(204).end();
-    });
+    })
+    .catch(error => next(error))
 });
+// Delete Method
+
+// Error Handler
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error);
+}
+
+app.use(errorHandler)
+// Error Handler
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
